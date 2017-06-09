ded86bf061d2bb3ba30909435fd8173ddb246d9c
Tabor H <thetabor@gmail.com>
2017-06-09 12:00:16 -0600

    Temporarily removing kulka components to set up neural net

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Sphero.Swarms
+# GoldStarSphero
+-![Segmentation test](https://raw.githubusercontent.com/thetabor/Sphero.Swarms/master/image_processing/images/segmentation/sphero_seg2.png)
 
-An exploration of swarm robotics. The base development swarm is three Sphero SPRK's, and a webcam. For initial tasks, I use a "hive mind" model, with a single neural net controlling the whole swarm. In the future, I'd like to have a development swarm with the capacity for ad-hoc cooperation. That is, a neural network for each agent in the swarm.
+An exploration of reinforcement learning with Deep-Q Networks using Sphero robotic balls.
 
 # Initial Goals
 
 The goals are behavioral in nature, rather than statistic.
-- **Simulation** : demonstrate reinforcement learning
+- **Simulation** : demonstrate reinforcement learning for simple navigation
 - **Navigation** : using reinforcement learning in a physical space
-- **Coordination** : collective navigation
-- **Cooperation** : swarm moves a target one could not move on its own
+- **Cooperation** : multiple agents move a target
 
 # Major utilities:
 
@@ -31,13 +31,22 @@ Coded with Python 3.6 on Mac OS X and Lubuntu 16ish.
 - Nervanasys blog post (linked in Karpathy):
     - [Deep Reinforcement Learning](https://www.nervanasys.com/demystifying-deep-reinforcement-learning/)
 
-# Methods & Data
+# Agents, Environments, and Reinforcement Learning
 
+- **Agents** 
 - **Supervised Learning** is the standard method of many statistical models and neural networks. It requires an (X, y) style training set, with inputs and desired outputs. For any kind of agent, this becomes a limitation, as the agent will be limited by the input data. That being said, we demonstrate supervised learning in the [Neural.Swarms](https://github.com/thetabor/Neural.Swarms) simulation. For the simple task of reaching a goal position in a deterministic environment, it performs very well after a short training period. We obtain data for this training from a deterministic strategy.
-- **Reinforcement Learning** is a method of training machine learning algorithms with rewards. Rather than learning from an (X, y) training set, it learns from experience. Each experience comes with certain rewards, and each time a reward is received, the algorithm can learn. A Deep-Q Network implements this method as neural network Q-estimator. A thorough explanation of the DQN and Q-estimation is available in the [references](https://www.nervanasys.com/demystifying-deep-reinforcement-learning/) above.
-- **RL Data:** At each step of the game, the agent experiences something and learns from it. Exactly what it is learning requires some subtle ideas. The reward is a 'ground truth' on which the agent can based its understanding of the world. But, we're interested in more than short-term rewards. We'd like to encourage the agent into complex, intelligent behavior. So, it must look forward. Enter the Q-value. A Q-value is a ** quality ** of a state, or the expected sum of rewards as we play the game from that state. Initially, the agent has absolutely no knowledge of the environment, so Q-values are effectively random. At each step, it updates the Q-value using the actual reward, plus the Q-value of the next step it plans on taking. So, our model fits (X, y) data, but each y is actually self-generated and often very innacurate. But since a part of it is ground truth, the model eventually learns something close enough to real Q-values to funtion.
--![Segmentation test](https://raw.githubusercontent.com/thetabor/Sphero.Swarms/master/image_processing/images/segmentation/sphero_seg2.png)
+- **Reinforcement Learning** is a method of training machine learning algorithms with rewards. Rather than learning from an (X, y) training set, it learns from experience. Each experience comes with certain rewards, and each time a reward is received, the algorithm can learn. A Deep-Q Network implements this method as neural network Q-estimator.
+- **Actions, and rewards**
+- **Markov Decision Processes**
+- **DQNs**
+- **RL Data:** At each step of the game, the agent experiences something and learns from it. Exactly what it is learning requires some subtle ideas. The reward is a 'ground truth' on which the agent can based its understanding of the world. But, we're interested in more than short-term rewards. We'd like to encourage the agent into complex, intelligent behavior. So, it must look forward. Enter the Q-value. A Q-value is a **quality** of a state, or the expected sum of rewards as we play the game from that state. Initially, the agent has absolutely no knowledge of the environment, so Q-values are effectively random. At each step, it updates the Q-value using the actual reward, plus the Q-value of the next step it plans on taking. So, our model fits (X, y) data, but each y is actually self-generated and often very innacurate. But since a part of it is ground truth, the model eventually learns something close enough to real Q-values to funtion.
+
+# Simulation
+
+Using the [Neural.Swarms](https://github.com/thetabor/Neural.Swarms) simulation engine I can implement either supervised or reinforcement learning. Here we see how quickly the supervised learner can perform well on the simple task. In contrast, the reinforcement learner does not yet perform well.
+
+# Supervised learning vs. Reinforcement Learning
+
 -![Game test](https://raw.githubusercontent.com/thetabor/Sphero.Swarms/master/notes/game_test_0.png)
 -![Game test](https://raw.githubusercontent.com/thetabor/Sphero.Swarms/master/notes/game_test_1.png)
 -![Game test](https://raw.githubusercontent.com/thetabor/Sphero.Swarms/master/notes/game_test_2.png)
-- **Simulation** Using the [Neural.Swarms](https://github.com/thetabor/Neural.Swarms) simulation engine I can implement either supervised or reinforcement learning. Here we see how quickly the supervised learner can perform well on the simple task. In contrast, the reinforcement learner does not yet perform well.
--- src/get_to_center.py
+++ src/get_to_center.py
@@ -14,13 +14,13 @@ from webcam_segmentation import *
 sys.path.append(os.path.abspath("../../kulka"))
 from kulka import Kulka
 
-# # neural network imports
-# from keras.utils import np_utils
-# from keras.models import Sequential
-# from keras.layers.core import Dense
-# from keras.optimizers import sgd, RMSprop, Adagrad, Adam
+# neural network imports
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import sgd, RMSprop, Adagrad, Adam
 
-def show_cv(regions, image, filename = "test"):
+def get_cv(regions, image, filename = "test"):
     '''
     IN: regions of an image, an image to display with it
     OUT: the position and goal of the Sphero
@@ -60,123 +60,101 @@ def show_cv(regions, image, filename = "test"):
     else:
         distance = 0.00
         reward = 0.00
+    output.append(reward)
     dist_str = "Distance: " + "{0:.2f}".format(distance)
     reward_str = "Reward: " + "{0:.2f}".format(reward)
     plt.title(reward_str)
     plt.title(dist_str, loc="left")
     plt.tight_layout()
+    # plt.show()
+    return plt, output
+
+def plot_cv(regions, image, filename = "test"):
+    plt, out = get_cv(regions, image, filename)
     plt.show()
-    return output
+    return out
 
-def get_cv(regions, image):
-    '''
-    IN: regions of an image, an image to display with it
-    OUT: the position and goal of the Sphero
-    '''
-    goal = (320, 240)
-    centroids = []
-    for region in regions:
-            # draw rectangle around segmented regions
-            minr, minc, maxr, maxc = region.bbox
-            centroid = (minc + 0.5*(maxc - minc), minr + 0.5*(maxr - minr))
-            centroids.append(centroid)
-
-    output = []
-    if len(centroids) > 0:
-        distance = dist_e(goal, centroids[0])
-        output.append(centroids[0])
-        output.append(goal)
-        if distance < 100.0:
-            reward = (100.0 - distance) / 100
-        else:
-            reward = -0.01
-    else:
-        distance = 0.00
-        reward = 0.00
-    output.append(reward)
-    dist_str = "Distance: " + "{0:.2f}".format(distance)
-    reward_str = "Reward: " + "{0:.2f}".format(reward)
-    return output
-
-def notebook_cv(n = 10):
-    cam = cam_setup()
-    for i in range(n):
-        display.clear_output(wait=True);
-        _ = capture_image(cam)
-        labelled_image, image, _ = segment_photo_bmp()
-        filtered_regions = filter_regions(labelled_image, min_area = 500)
-        display.display(show_cv(filtered_regions, image));
-    cam_quit(cam)
-
-def step_game(kulka, cam, n=1, model=None):
-    kulka.set_rgb(0, 0, 0xFF)
-    time.sleep(0.1)
+def step_game(sphero, cam, model=None, notebook = False):
+    # kulka.set_rgb(0, 0, 0xFF)
+    # time.sleep(0.1)
     _ = capture_image(cam)
     labelled_image, image, _ = segment_photo_bmp()
     filtered_regions = filter_regions(labelled_image, min_area = 500)
-    data = get_cv(filtered_regions, image)
-    inputs = []
-    predicts = []
+    if notebook == True:
+        data = plot_cv(filtered_regions, image)
+    else:
+        plt, data = get_cv(filter_regions, image)
     if len(data) == 2:
         # special sauce goes here
         if model!=None:
-            inputs.append(data)
-            predicts.append(str(model.predict(data)))
+            inputs = data
+            predicts = model.predict(data)
             # direction = out[0]*360
             # speed = out[1]*60
         else:
             direction = randint(0, 359)
             speed = randint(0, 45)
             inputs.append("random")
-        kulka.roll(speed, direction)
-    kulka.set_rgb(0, 0, 0)
-    time.sleep(0.1)
-    kulka.set_rgb(0xFF, 0, 0)
-    return inputs, predicts
+    #     kulka.roll(speed, direction)
+    # kulka.set_rgb(0, 0, 0)
+    # time.sleep(0.1)
+    # kulka.set_rgb(0xFF, 0, 0)
+    if notebook == False:
+        return inputs, predicts, plt
 
-def display_game(sphero, cam, n = 5):
+def notebook_game(cam, sphero = None, n = 5):
+    # with Kulka(addrs[0]) as kulka:
+    log = []
+    # kulka.set_inactivity_timeout(300)
+    cam = cam_setup()
+    model = baseline_model()
+    # inputs number of rounds
     for i in range(n):
         display.clear_output(wait=True);
-        display.display(step_game(sphero, cam));
+        display.display(step_game(sphero, cam, notebook = True));
+    # kulka.close()
+    cam_quit(cam)
 
-# def baseline_model(optimizer = Adam(),
-#                     layers = [{"size":20,"activation":"relu"}]):
-#     # four inputs - each coordinate
-#     inputs = 4
-#     # seven outputs - one for each action
-#     # going with a hexagon of unit movements since this is
-#     # discritized by our structure
-#     num_outputs = 7
-#     # prepare the navigator model
-#     model = Sequential()
-#     # initial inputs
-#     l = list(layers)
-#     l0 = l[0]
-#     del l[0]
-#     model.add(Dense(l0['size'],
-#                     input_dim = inputs,
-#                     activation = l0['activation']))
-#     # the hidden layers
-#     for layer in l:
-#         model.add(Dense(layer['size'], activation=layer['activation']))
-#     # the output layer
-#     model.add(Dense(num_outputs, activation='linear'))
-#     model.compile(optimizer = optimizer,
-#                     loss = "mean_squared_error")
-#     return model
+def baseline_model(optimizer = Adam(),
+                    layers = [{"size":20,"activation":"relu"}]):
+    # four inputs - each coordinate
+    inputs = 4
+    # five outputs - one for each action
+    # going with a square of unit movements since this is
+    # discritized by our structure
+    num_outputs = 5
+    # prepare the navigator model
+    model = Sequential()
+    # initial inputs
+    l = list(layers)
+    l0 = l[0]
+    del l[0]
+    model.add(Dense(l0['size'],
+                    input_dim = inputs,
+                    activation = l0['activation']))
+    # the hidden layers
+    for layer in l:
+        model.add(Dense(layer['size'], activation=layer['activation']))
+    # the output layer
+    model.add(Dense(num_outputs, activation='linear'))
+    model.compile(optimizer = optimizer,
+                    loss = "mean_squared_error")
+    return model
 
 def play_game(addrs = ['68:86:E7:06:FD:1D']):
-    with Kulka(addrs[0]) as kulka:
-        log = []
-        kulka.set_inactivity_timeout(300)
-        cam = cam_setup()
-        model = None #baseline_model()
-        # inputs number of rounds
-        for i in range(5):
-            # input turns for this round
-            log.append(step_game(kulka, cam, n = 1, model = model))
-        kulka.close()
-        cam_quit(cam)
+    # with Kulka(addrs[0]) as kulka:
+    sphero = None
+    log = []
+    # kulka.set_inactivity_timeout(300)
+    cam = cam_setup()
+    model = baseline_model()
+    # inputs number of rounds
+    for i in range(5):
+        # input turns for this round
+        log = step_game(sphero, cam, model = model)
+        log.append([log[0], log[1]])
+    # kulka.close()
+    cam_quit(cam)
     return log
 
 if __name__ == "__main__":
@@ -184,5 +162,5 @@ if __name__ == "__main__":
     _ = capture_image(cam)
     labelled_image, image, _ = segment_photo_bmp()
     filtered_regions = filter_regions(labelled_image, min_area = 500)
-    show_cv(filtered_regions, image)
+    plot_cv(filtered_regions, image)
     cam_quit(cam)
