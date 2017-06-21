0813e187b092082014832778d4a0523079ae1940
Tabor C H <tabor.henderson@gmail.com>
2017-06-21 10:53:48 -0600

    code stubs for NeoKulka

--- src/supervised_data_gen.py
+++
@@ -1,8 +0,0 @@
-'''
-This file generates data for supervised training of a neural network for Sphero.
-The inputs are the five variables kulka.data_poll() returns:
-    xpos, ypos, xvel, yvel, sog
-where
-    sog ~= sqrt(xvel**2 + yvel**2)
-The outputs are speed, heading, and time.
-'''
